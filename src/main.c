bbaea549a4a2485a1e28b46380facde795f0b482
Matteo Ricciotti <ricciottimatteo2003@gmail.com>
2025-12-18 16:00:52 +0100

    handle multiple paths

--- src/main.c
+++ src/main.c
@@ -7,11 +7,15 @@
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
+#define REQUEST_METHOD_SIZE 8
+#define REQUEST_PATH_SIZE 256
+#define REQUEST_VERSION_SIZE 16
+#define REQUEST_BODY_SIZE 256
 #define MAX_CLIENT_QUEUE 10
 
 int main()
 {
-    // 1. Create the server socket and return a file descriptor
+    // Create the server socket and return a file descriptor
     // AF_INET = IPv4, SOCK_STREAM = TCP, 0 = default protocol
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -21,7 +25,7 @@ int main()
         return 1;
     }
 
-    // 2. Create the address structure for the binding
+    // Create the address structure for the binding
     struct sockaddr_in serverAddress;
     // Start filling the memory at address <param1>, with value, <param2>, for <param3> bytes
     memset(&serverAddress, 0, sizeof(serverAddress));
@@ -30,14 +34,14 @@ int main()
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     serverAddress.sin_port = htons(PORT);
 
-    // 3. Bind the socket to the IP + port
+    // Bind the socket to the IP + port
     if (-1 == bind(server_fd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)))
     {
         perror("Error while binding the socket to the IP + port");
         return 1;
     }
 
-    // 4. Make the server listen to the socket
+    // Make the server listen to the socket
     // 10 is the max connections queue size
     if (-1 == listen(server_fd, MAX_CLIENT_QUEUE))
     {
@@ -47,10 +51,10 @@ int main()
 
     printf("Server listening on port %d\n", PORT);
 
-    // 5. Infinte loop to accept connections
+    // Infinte loop to accept connections
     while (1)
     {
-        // 6. Create the socket of a client connection
+        // Create the socket of a client connection
         int client_fd = accept(server_fd, NULL, NULL);
 
         if (-1 == client_fd)
@@ -61,12 +65,12 @@ int main()
 
         printf("Client %d connected!\n", client_fd);
 
-        // 7. Read client data
-        char buffer[BUFFER_SIZE] = "";
+        // Read client data
+        char requestBuffer[BUFFER_SIZE] = "";
 
-        // recv() blocks the execution until some data get received
+        // recv() blocks the execution until some data is received
         // -1 is for '\0'
-        int bytes_read = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+        int bytes_read = recv(client_fd, requestBuffer, BUFFER_SIZE - 1, 0);
 
         if (-1 == bytes_read)
         {
@@ -82,10 +86,51 @@ int main()
             continue;
         }
 
-        printf("Received: %s\n", buffer);
+        printf("Received HTTP request:\n%s\n", requestBuffer);
 
-        // 8. Echo
-        int bytes_sent = send(client_fd, buffer, bytes_read, 0);
+        // Parse the request elements
+        char method[REQUEST_METHOD_SIZE] = "";
+        char path[REQUEST_PATH_SIZE] = "";
+        char version[REQUEST_VERSION_SIZE] = "";
+
+        sscanf(requestBuffer, "%s %s %s", method, path, version);
+
+        printf("Method: %s, Path: %s, Version: %s\n", method, path, version);
+
+        // Create the formatted response
+        char responseBuffer[BUFFER_SIZE];
+
+        char *responseMessage = "200 OK";
+        char *body;
+
+        if (0 == strcmp("/", path))
+        {
+            body = "Hello World";
+        }
+        else if (0 == strcmp("/about", path))
+        {
+            body = "About";
+        }
+        else
+        {
+            responseMessage = "404 Not Found";
+            body = "Not Found";
+        }
+
+        sprintf(responseBuffer,
+                "HTTP/1.1 %s\r\n"
+                "Content-Type: text/plain\r\n"
+                "Content-Length: %ld\r\n"
+                "\r\n"
+                "%s",
+                responseMessage,
+                strlen(body),
+                body);
+
+        printf("Response:\n%s\n", responseBuffer);
+
+        // Send the response
+        int bytes_sent = send(client_fd, responseBuffer, strlen(responseBuffer), 0);
 
         if (-1 == bytes_sent)
         {
@@ -94,13 +139,13 @@ int main()
             continue;
         }
 
-        // 9. Close client socket
+        // Close client socket
         close(client_fd);
 
         printf("Connection to the client %d closed\n\n", client_fd);
     }
 
-    // 10. close the server socket (will not be reached)
+    // Close the server socket (will not be reached)
     close(server_fd);
 
     return 0;
